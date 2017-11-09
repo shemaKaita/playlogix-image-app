4271d1992d3bbe758dc0242101b62442236af9c7
shema <skaitakirwa@gmail.com>
2017-11-09 11:42:55 +0200

    db dump and images

--- README.md
+++ README.md
@@ -13,8 +13,6 @@ Setup
  1. Clone the repo
  2. Run `composer install`
  3. Then run `yarn`
- 5. Then you will need to populate the db with dummy data
- 6. Add the images (make sure to place them in `/public/assets/images`
  7. Make sure to fill the `/sample-settings.json` and `sample-secrets.json` with your settings. Also make sure to rename and the file without the `sample-` in it
  7. Then finally run `gulp` to start
  
---
+++ resources/db-dump-09-11-2017.sql
@@ -0,0 +1,113 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Nov 09, 2017 at 10:40 AM
+-- Server version: 5.6.35
+-- PHP Version: 7.0.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `playlogix`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `images`
+--
+
+CREATE TABLE `images` (
+  `id` mediumint(9) NOT NULL,
+  `url` varchar(2083) NOT NULL,
+  `name` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `images`
+--
+
+INSERT INTO `images` (`id`, `url`, `name`) VALUES
+(1, './dist/images/hotrod-2918174_1280.jpg', 'hotrod'),
+(2, './dist/images/iphone-2854327_1280.png', 'phone'),
+(3, './dist/images/airbus-2132610_1280.jpg', 'airbus'),
+(4, './dist/images/meat-709346_1280.jpg', 'cooking pan'),
+(5, './dist/images/avenue-2215317_1280.jpg', 'trees');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+CREATE TABLE `tags` (
+  `id` mediumint(9) NOT NULL,
+  `name` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tags`
+--
+
+INSERT INTO `tags` (`id`, `name`) VALUES
+(1, 'nature'),
+(2, 'technology'),
+(3, 'appliances'),
+(4, 'travel');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tag_relationships`
+--
+
+CREATE TABLE `tag_relationships` (
+  `tag_id` mediumint(9) NOT NULL,
+  `image_id` mediumint(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tag_relationships`
+--
+
+INSERT INTO `tag_relationships` (`tag_id`, `image_id`) VALUES
+(2, 2),
+(4, 1),
+(4, 3),
+(3, 4),
+(1, 5),
+(2, 3);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tags`
+--
+ALTER TABLE `tags`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `images`
+--
+ALTER TABLE `images`
+  MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `tags`
+--
+ALTER TABLE `tags`
+  MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
